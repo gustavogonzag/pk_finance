26ff9ef860d0082f391b01acaa511801cc176c16
Gustavo Gonzaga <gustavosg91@gmail.com>
2024-05-28 16:36:43 -0300

    feat: save in localstorage

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -1,20 +1,17 @@
 function resume() {
     return (
-        <div className="flex flex-wrap justify-center mt-10">
+        <div className="flex flex-wrap justify-center mt-10 -ml-28">
             <div className="flex flex-row gap-8 -ml-96">
                 <div>
-                    <span className="text-gray-light font-medium">XX no total <br /></span>
+                    <span className="text-gray-light font-medium">Total <br /></span>
                     <span className="text-[#FFF] font-medium">R$ 8.523,00</span>
                 </div>
                 <div>
-                    <span className="text-[#FF0000] font-medium">XX em atraso <br /></span>
+                    <span className="text-[#FF0000] font-medium">Total débitos <br /></span>
                     <span className="text-[#FFF] font-medium">R$ 8.523,00</span>
                 </div>
                 <div>
-                    <span className="text-[#098007] font-medium">XX pagas <br /></span>
-                    <span className="text-[#FFF] font-medium">R$ 8.523,00</span>
-                </div><div>
-                    <span className="text-gray-light font-medium">XX a pagar <br /></span>
+                    <span className="text-[#098007] font-medium">Total crédito <br /></span>
                     <span className="text-[#FFF] font-medium">R$ 8.523,00</span>
                 </div>
             </div>
--- src/components/sessionAddContent.tsx
+++ src/components/sessionAddContent.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 function SessionAddContent() {
 
+    const [descricao, setDescricao] = useState<string>("");
+    const [data, setData] = useState<string>("");
+    const [valor, setValor] = useState<string>("");
     const [selectedOption, setSelectedOption] = useState<string | null>(null);
 
     const handleOptionChange = (option: string) => {
@@ -9,12 +12,34 @@ function SessionAddContent() {
         setSelectedOption(selectedOption === option ? null : option);
     };
 
+    const handleAdd = (event: { preventDefault: () => void; }) => {
+        event.preventDefault();
+        const receita = {
+            descricao: descricao,
+            data: data,
+            valor: valor,
+            selectedOption: selectedOption
+        }
+        const registrosFinanceiros = [
+            ...JSON.parse(localStorage.getItem('registrosFinanceiros') || '[]'),
+            receita
+        ]
+        addSaveStorage(registrosFinanceiros);
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
+    const addSaveStorage = (registrosFinanceiros: any) => {
+
+        localStorage.setItem('registrosFinanceiros', JSON.stringify(registrosFinanceiros));
+        console.log(registrosFinanceiros);
+    }
+
     return (
         <div className="flex justify-center mt-10">
             <div className="flex flex-wrap justify-center items-center bg-[#242424] w-auto rounded-md gap-4 px-10 py-4">
-                <input className="p-2 rounded-md bg-gray-dark text-[#FFF]" placeholder="Digite aqui a descricão" type="text" />
-                <input className="p-2 rounded-md  bg-gray-dark text-[#FFF]" type="date" />
-                <input className="p-2 w-24 rounded-md bg-gray-dark text-[#FFF]" placeholder="Valor" type="text" />
+                <input className="p-2 rounded-md bg-gray-dark text-[#FFF]" placeholder="Digite aqui a descricão" type="text" onChange={(e) => setDescricao(e.target.value)} value={descricao} />
+                <input className="p-2 rounded-md  bg-gray-dark text-[#FFF]" type="date" onChange={(e) => setData(e.target.value)} value={data} />
+                <input className="p-2 w-24 rounded-md bg-gray-dark text-[#FFF]" placeholder="Valor" type="text" onChange={(e) => setValor(e.target.value)} value={valor} />
                 <div className="text-[#FFF]">
                     <input className="mr-2" type="radio" name="credito" id="credito" checked={selectedOption === 'credito'}
                         onChange={() => handleOptionChange('credito')} />
@@ -25,7 +50,7 @@ function SessionAddContent() {
             onChange={() => handleOptionChange('debito')} />
                     Débito
                 </div>
-                <button className="bg-orange rounded-md px-4 text-[#FFF] font-medium ml-4 w-36 h-11">Adicionar</button>
+                <button className="bg-orange rounded-md px-4 text-[#FFF] font-medium ml-4 w-36 h-11" onClick={handleAdd}>Adicionar</button>
             </div>
         </div>
     )
